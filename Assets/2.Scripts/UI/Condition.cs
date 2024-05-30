c93c360f059811420c383719fcea94d07432ef14
unm7925 <unm7925@gmail.com>
2024-05-30 21:28:30 +0900

    Add All

---
+++ Assets/2.Scripts/UI/Condition.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Condition : MonoBehaviour
+{
+    public float curValue;
+    public float startValue;
+    public float maxValue;
+    public float passiveValue;
+    public Image uiBar;
+    void Start()
+    {
+        curValue = startValue;
+    }
+
+
+    void Update()
+    {
+        // ui update
+        uiBar.fillAmount = GetPercentage();
+    }
+
+    float GetPercentage()
+    {
+        return curValue / maxValue;
+    }
+
+    public void Add(float value)
+    {
+        curValue = Mathf.Min(curValue + value, maxValue);
+    }
+
+    public void Subtract(float value)
+    {
+        curValue = Mathf.Max(curValue - value, 0);
+    }
+}
